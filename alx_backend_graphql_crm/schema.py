a25c1a2bff62de78218f1542817259b50d4642a8
goShell <herkodandy@gmail.com>
2025-06-29 23:33:41 +0300

    feat: implementation for adding filtering capabilities to your GraphQL API using django-filter and graphene-django

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -1,11 +1,7 @@
+from crm.schema import Query as CrmQuery
 import graphene
 
-
-class Query(graphene.ObjectType):
-    hello = graphene.String()
-
-    def resolve_hello(root, info):
-        return "Hello, GraphQL!"
-
+class Query(CrmQuery, graphene.ObjectType):
+    pass
 
 schema = graphene.Schema(query=Query)
