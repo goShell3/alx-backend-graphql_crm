4ed74b721750f04c15e1e6d428aa832b4101cda3
goShell <herkodandy@gmail.com>
2025-06-29 23:37:57 +0300

    feat: basic GraphQL endpoint using graphene-django

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -1,7 +1,16 @@
-from crm.schema import Query as CrmQuery
+# from crm.schema import Query as CrmQuery
+# import graphene
+
+# class Query(CrmQuery, graphene.ObjectType):
+#     pass
+
+# schema = graphene.Schema(query=Query)
 import graphene
 
-class Query(CrmQuery, graphene.ObjectType):
-    pass
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(root, info):
+        return "Hello, GraphQL!"
 
 schema = graphene.Schema(query=Query)
